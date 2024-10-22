d77e2bcce3e81021df06e908df6a01551bca497f
Gur Telem <gtelem@apple.com>
2024-10-22 11:41:57 +0300

    Replace selenium with direct requests using curl_cffi.requests for faster and more reliable polling

--- hilan.py
+++ hilan.py
@@ -4,131 +4,119 @@
 # The cronjob should be something like (every 15 minutes):
 # */15 * * * * cd <path_to_repo> ; /usr/bin/env venv/bin/python hilan.py
 
-import time
+from subprocess import check_output
 import os
 import json
 from datetime import datetime
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
-# Add logging to file
 import logging
+from curl_cffi import requests
+
 # Config the log to include the time, level, and message
 logging.basicConfig(filename='last_run_logs.log', level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-
-
 file_indicator = 'hilan.txt'
-
-# Get current month and year
 current_month = datetime.now().month
-year = datetime.now().year
 
-# Check if we already sent a notification for this month
-if os.path.exists(file_indicator):
-    with open(file_indicator, 'r') as f:
-        if f.read() == f'{current_month}':
-            logging.info(f'Already sent notification for month {current_month}')
-            exit(0)
 
-options = Options()
-options.add_argument('-headless')
-options.headless = True
+def login():
+    password = get_password()
+    url = "https://apple.net.hilan.co.il/HilanCenter/Public/api/LoginApi/LoginRequest"
 
-logging.info(f'Checking if payslip is available for month {current_month}')
-driver = webdriver.Firefox(options=options)
+    payload = f'orgId=1047&username=632382&password={password}&isEn=true'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+    session = requests.Session()
+    response = session.post(url, headers=headers, data=payload, max_redirects=3, timeout=5).json()
+    if response['IsFail']:
+        logging.error(f'Login error: {response}')
+        return None
 
-# link to open a site
-driver.get("https://apple.net.hilan.co.il/")
+    return session
 
-# 10 sec wait time to load, if good internet connection is not good then increase the time
-# units in seconds
-# note this time is being used below also
-wait25 = WebDriverWait(driver, 25)
-wait10 = WebDriverWait(driver, 10)
-wait5 = WebDriverWait(driver, 5)
-# Wait for text input #user_nm to load
-try:
-    username_input = wait5.until(EC.presence_of_element_located((
-        By.XPATH, '//input[@id="user_nm"]'
-    )))
-except Exception as e:
-    logging.error(f'Failed to find username input: {e}')
-    raise e
 
-# Wait for text input #password_nm to load
-try:
-    password_input = wait5.until(EC.presence_of_element_located((
-        By.XPATH, '//input[@id="password_nm"]'
-    )))
-except Exception as e:
-    logging.error(f'Failed to find password input: {e}')
-    raise e
+def get_data(session):
+    url = "https://apple.net.hilan.co.il/Hilannetv2/Services/Public/WS/HSalaryApiapi.asmx/GetData"
 
-# Load credentials from creds.json
-try:
-    with open('creds.json', 'r') as f:
-        creds = json.load(f)
-except Exception as e:
-    logging.error(f'Failed to load creds.json: {e}')
-    raise e
+    payload = {
+        "IsLastPayslipMonth": True
+    }
+    response = session.post(url, data=payload)
+    return response.json()
 
-# Enter credentials
-username = creds['username']
-password = creds['password']
 
-try:
-    username_input.send_keys(username)
-    password_input.send_keys(password)
-    password_input.send_keys(Keys.ENTER)
-except Exception as e:
-    logging.error(f'Failed to enter credentials: {e}')
-    raise e
+def is_current_month_available():
+    session = login()
 
-#  Wait for div with inner text " תקציר שכר " to load
-try:
-    wait5.until(EC.presence_of_element_located((
-        By.XPATH, '//div[contains(text(),"תקציר שכר")]'
-    )))
-except Exception as e:
-    logging.error(f'Failed to find salary summary: {e}')
-    raise e
+    if not session:
+        logging.error('Failed to login to HilaNet')
+        return False
 
-# Find a sibling element with class .month-year-wrapper
-try:
-    month_year_wrapper = wait5.until(EC.presence_of_element_located((
-        By.XPATH, '//div[contains(text(),"תקציר שכר")]/following-sibling::div[contains(@class,"month-year-wrapper")]'
-    )))
-except Exception as e:
-    logging.error(f'Failed to find month-year-wrapper: {e}')
-    raise e
+    data = get_data(session)
+    ans = False
+    if data['Month'] == current_month:
+        ans = True
 
-# Find the first <option> that element
-try:
-    first_option = wait5.until(EC.presence_of_element_located((
-        By.XPATH, './/option[1]')))
-except Exception as e:
-    logging.error(f'Failed to find first option: {e}')
-    raise e
+    logging.info(f'Is new payslip is available for month {current_month}? {"Yes" if ans else "No"} (last = {data["Month"]})')
+    return ans
 
-available_months = [x for x in month_year_wrapper.text.split('\n') if str(year) in x]
-driver.quit()
-del driver
-if (len(available_months)) == current_month:
-    logging.info(f'Found payslip for month {current_month}')
+
+def was_curr_month_last_reported_month():
     # Check if we already sent a notification for this month
     if os.path.exists(file_indicator):
         with open(file_indicator, 'r') as f:
             if f.read() == f'{current_month}':
-                exit(0)
-    os.system(f"""osascript -e 'display notification "✅💵💰 Hilan has this month: {len(available_months)}'"'"'s payslip" with title "Hilan Payslip Watch" sound name "Submarine"'""")
-    os.system(f'''osascript -e 'tell application "Shortcuts Events" to run the shortcut named "Send Payslip Notification Message"' ''')
+                logging.debug(f'Already sent notification for month {current_month}')
+                return True
+
+    return False
+
+
+def persist_current_month_as_reported():
     # Save an indication that we found this month's payslip already
     with open(file_indicator, 'w') as f:
         f.write(f'{current_month}')
-else:
-    logging.info(f'No payslip for month {current_month} yet')
-    # os.system(f"""osascript -e 'display notification "❌ Hilan last payslip month is {len(available_months)}" with title "Hilan Payslip Watch"'""")
 
+
+def report_current_month():
+    os.system(
+        f"""osascript -e 'display notification "✅💵💰 Hilan has this month: {current_month}'"'"'s payslip" with title "Hilan Payslip Watch" sound name "Submarine"'""")
+    # os.system(f'''osascript -e 'tell application "Shortcuts Events" to run the shortcut named "Send Payslip Notification Message"' ''')
+
+    persist_current_month_as_reported()
+
+
+def get_password():
+    # Load credentials from creds.json
+    try:
+        with open('creds.json', 'r') as f:
+            creds = json.load(f)
+    except Exception as e:
+        logging.error(f'Failed to load creds.json: {e}')
+        raise e
+
+    if 'password' in creds:
+        password = creds['password']
+    else:
+        onepass_output = check_output(f"op item get {creds['1pass_item_id']} --fields label=username,label=password", shell=True).decode()
+        password = onepass_output[onepass_output.find(',') + 1:].strip()
+
+    return password
+
+
+def is_new_payslip_available():
+    if was_curr_month_last_reported_month():
+        return False
+
+    if is_current_month_available():
+        return True
+
+    return False
+
+
+def main():
+    if is_new_payslip_available():
+        report_current_month()
+
+
+if __name__ == '__main__':
+    main()
